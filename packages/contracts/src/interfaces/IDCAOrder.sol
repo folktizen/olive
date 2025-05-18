a1ad887661e449114c00802430ef8afe06e5ff51
Ainul Ξ Creative Dev <hey@moojok.space>
2025-05-18 21:34:24 +0700

    Refactor OrderFactory and related contracts for improved readability and consistency
    - Adjusted indentation and formatting for better code clarity in OrderFactory.sol.
    - Enhanced documentation comments for functions in OrderFactory.sol, EIP1271Verifier.sol, IConditionalOrder.sol, IDCAOrder.sol, IGPv2Settlement.sol, and GPv2Order.sol.
    - Streamlined function signatures and internal logic for consistency across contracts.
    - Ensured proper use of error handling and revert messages where applicable.
    - Updated event emissions and function parameters for clarity and maintainability.

--- packages/contracts/src/interfaces/IDCAOrder.sol
+++ packages/contracts/src/interfaces/IDCAOrder.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.20;
 pragma abicoder v2;
 
 interface IDCAOrder {
-    function initialize(
-        address _owner,
-        address _receiver,
-        address _sellToken,
-        address _buyToken,
-        uint256 _amount,
-        uint256 _startTime,
-        uint256 _endTime,
-        uint256 _interval,
-        address _settlementContract
-    ) external returns (bool);
+  function initialize(
+    address _owner,
+    address _receiver,
+    address _sellToken,
+    address _buyToken,
+    uint256 _amount,
+    uint256 _startTime,
+    uint256 _endTime,
+    uint256 _interval,
+    address _settlementContract
+  ) external returns (bool);
 }
--- packages/contracts/src/interfaces/IGPv2Settlement.sol
+++ packages/contracts/src/interfaces/IGPv2Settlement.sol
@@ -3,12 +3,12 @@
 pragma solidity ^0.8.20;
 
 interface IGPv2Settlement {
-    /// @dev Sets a presignature for the specified order UID.
-    ///
-    /// @param orderUid The unique identifier of the order to pre-sign.
-    function setPreSignature(bytes calldata orderUid, bool signed) external;
+  /// @dev Sets a presignature for the specified order UID.
+  ///
+  /// @param orderUid The unique identifier of the order to pre-sign.
+  function setPreSignature(bytes calldata orderUid, bool signed) external;
 
-    function vaultRelayer() external view returns (address);
+  function vaultRelayer() external view returns (address);
 
-    function domainSeparator() external view returns (bytes32);
+  function domainSeparator() external view returns (bytes32);
 }
