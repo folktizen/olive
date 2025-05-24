8317dc666655e72af35a39fafbdf71a56428bc02
Ainul Ξ Creative Dev <hey@moojok.space>
2025-05-24 10:04:21 +0700

    feat: remove mainnet references from configuration and update build scripts for supported networks

--- packages/subgraph/bin/config.ts
+++ packages/subgraph/bin/config.ts
@@ -7,34 +7,28 @@ export const config: Record<
     };
   }
 > = {
-  mainnet: {
-    orderFactory: {
-      address: "0x01c4B69C897C8936B7F0E361d91e2C78F35082A5",
-      startBlock: 22527750,
-    },
-  },
   optimism: {
     orderFactory: {
-      address: "0xefc25aa869B78DcF9A33De4D39bD09C6B6F916dF",
-      startBlock: 136062765,
+      address: "0xf754a9e276F4f1D385e9515Ec8bEbf6c00C0BB0A",
+      startBlock: 136228273,
     },
   },
   "arbitrum-one": {
     orderFactory: {
-      address: "0xb31BA189b09F82C8920E7f7144e81dFfE85fFBa6",
-      startBlock: 339477623,
+      address: "0xf754a9e276F4f1D385e9515Ec8bEbf6c00C0BB0A",
+      startBlock: 339895459,
     },
   },
   matic: {
     orderFactory: {
-      address: "0x2884Fb2b529Cff99906E2521DF6328EEEfFdeea8",
-      startBlock: 71739422,
+      address: "0xf754a9e276F4f1D385e9515Ec8bEbf6c00C0BB0A",
+      startBlock: 71892196,
     },
   },
   base: {
     orderFactory: {
-      address: "0x61d1C74DA0bdA3b26bC54Dc742Cf0bb88dCb8122",
-      startBlock: 30467493,
+      address: "0xf754a9e276F4f1D385e9515Ec8bEbf6c00C0BB0A",
+      startBlock: 30633015,
     },
   },
 };
