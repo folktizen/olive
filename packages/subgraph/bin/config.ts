ec7084f0e55683ce2bbb24837afec1ebe0c11bd9
Ainul Ξ Creative Dev <hey@moojok.space>
2025-05-30 08:11:52 +0700

    feat: add support for Gnosis network in build and deployment scripts; update order factory addresses

--- packages/subgraph/bin/config.ts
+++ packages/subgraph/bin/config.ts
@@ -7,16 +7,34 @@ export const config: Record<
     }
   }
 > = {
+  mainnet: {
+    orderFactory: {
+      address: "0x0000000000000000000000000000000000000000",
+      startBlock: 0
+    }
+  },
+  matic: {
+    orderFactory: {
+      address: "0x0000000000000000000000000000000000000000",
+      startBlock: 0
+    }
+  },
   "arbitrum-one": {
     orderFactory: {
-      address: "0x8B30d482FB833813e3db9f6D96516e31763df3b8",
-      startBlock: 341027196
+      address: "0x0000000000000000000000000000000000000000",
+      startBlock: 0
     }
   },
   base: {
     orderFactory: {
-      address: "0x8B30d482FB833813e3db9f6D96516e31763df3b8",
-      startBlock: 30774887
+      address: "0x0000000000000000000000000000000000000000",
+      startBlock: 0
+    }
+  },
+  gnosis: {
+    orderFactory: {
+      address: "0x0000000000000000000000000000000000000000",
+      startBlock: 0
     }
   }
 }
