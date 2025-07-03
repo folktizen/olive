6d7cbe02c9d2e9bc62ab5430297c6db319f99916
Ainul Ξ Creative Dev <hey@moojok.space>
2025-07-03 15:34:49 +0700

    feat: Add meta tags for various generators in RootLayout component

--- packages/app/app/layout.tsx
+++ packages/app/app/layout.tsx
@@ -47,6 +47,35 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" className={stabilGrotesk.variable}>
+      <head>
+        {/* WTF Meta Tag */}
+        <meta name="GENERATOR" content="Astro v3.0.9" />
+        <meta name="GENERATOR" content="Jekyll v4.3.2" />
+        <meta name="GENERATOR" content="Halo 1.5.4" />
+        <meta name="GENERATOR" content="Nicepage 4.2.6" />
+        <meta name="GENERATOR" content="WordPress 6.4.1" />
+        <meta name="GENERATOR" content="Site Kit by Google 1.113.0" />
+        <meta
+          name="GENERATOR"
+          content="AMP Plugin v2.5.0; mode=standard; sandboxing-level=3:3"
+        />
+        <meta name="GENERATOR" content="DotNetNuke" />
+        <meta name="GENERATOR" content="OctoberCMS" />
+        <meta name="GENERATOR" content="Octopress" />
+        <meta name="GENERATOR" content="Mambo" />
+        <meta name="GENERATOR" content="Jalios" />
+        <meta name="GENERATOR" content="Zinnia" />
+        <meta name="GENERATOR" content="SEOmatic" />
+        <meta name="GENERATOR" content="ImpressCMS" />
+        <meta name="GENERATOR" content="PHP-Nuke" />
+        <meta name="GENERATOR" content="CMS Made Simple" />
+        <meta
+          name="GENERATOR"
+          content="Powered by Visual Composer Website Builder"
+        />
+        <meta name="GENERATOR" content="Rock v1.16.0.5" />
+        <meta name="GENERATOR" content="Microsoft FrontPage 3.0" />
+      </head>
       <body className="font-sans bg-fixed bg-no-repeat bg-100-100 bg-surface-25 bg-matrix-and-green-gradient text-em-high">
         <RybbitAnalytics />
         <Providers>
